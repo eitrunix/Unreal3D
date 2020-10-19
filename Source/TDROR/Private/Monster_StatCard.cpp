06ef9eceadb806b3ece9595fa1932f0ccbb6aac7
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-10-19 13:54:54 -0400

    Spawn Point Updates
    Adding in the Spawning Manager, Hopefully should be done this by wednesday

--- Source/TDROR/Private/Monster_StatCard.cpp
+++ Source/TDROR/Private/Monster_StatCard.cpp
@@ -9,30 +9,11 @@ UMonster_StatCard::UMonster_StatCard()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	currentSpawnCost = baseSpawnCost;
 
 	// ...
 }
 
 
-int UMonster_StatCard::GetBaseCost()
-{
-	return currentSpawnCost;
-}
-
-void UMonster_StatCard::SetCost(int _newCost)
-{
-	currentSpawnCost = _newCost;
-}
-
-int UMonster_StatCard::IncreaseCost(float _coeff)
-{
-	int newTmpCost;
-	newTmpCost = baseSpawnCost * _coeff;
-	SetCost(newTmpCost);
-
-	return newTmpCost;
-}
 
 // Called when the game starts
 void UMonster_StatCard::BeginPlay()
--- Source/TDROR/Public/Monster_StatCard.h
+++ Source/TDROR/Public/Monster_StatCard.h
@@ -6,44 +6,6 @@
 #include "Components/ActorComponent.h"
 #include "Monster_StatCard.generated.h"
 
-UENUM()
-enum MonsterTier
-{
-	Tier1		UMETA(DisplayName = "Tier1"),
-	Tier2	    UMETA(DisplayName = "Tier2"),
-	Tier3		UMETA(DisplayName = "Tier3"),
-	Boss		UMETA(DisplayName = "Boss"),
-};
-UENUM()
-enum MonsterStat
-{
-	RewardEXP		UMETA(DisplayName = "RewardEXP"),
-	RewardCurrency	UMETA(DisplayName = "RewardCurrency"),
-	AttackSpeed	    UMETA(DisplayName = "AttackSpeed"),
-	MoveSpeed		UMETA(DisplayName = "MoveSpeed"),
-	Damage			UMETA(DisplayName = "Damage"),
-	MAX_Health		UMETA(DisplayName = "MAX_Health"),
-	Health			UMETA(DisplayName = "Health"),
-
-};
-
-USTRUCT(BlueprintType)
-struct FMonsterStatData
-{
-	GENERATED_BODY()
-
-		MonsterStat Stat;
-		float BaseValue;
-		float CurrentValue;
-		int CurrentLevel;
-
-		float IncreaseStatByLevel()
-		{
-			CurrentValue = CurrentLevel * BaseValue;
-
-			return CurrentValue;
-		}
-};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TDROR_API UMonster_StatCard : public UActorComponent
@@ -54,22 +16,19 @@ public:
 	// Sets default values for this component's properties
 	UMonster_StatCard();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster Stats | Cost to Spawn")
-		int baseSpawnCost;	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster Stats | Cost to Spawn")
-		int currentSpawnCost;
-
-	UFUNCTION(BlueprintCallable, Category = "Monster Stats | Cost to Spawn")
-		int GetBaseCost();
-	UFUNCTION(BlueprintCallable, Category = "Monster Stats | Cost to Spawn")
-		void SetCost(int _newCost);	
-	UFUNCTION(BlueprintCallable, Category = "Monster Stats | Cost to Spawn")
-		int IncreaseCost(float _coeff);	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats | Per Level")
+		float healthPerLevel;	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats | Per Level")
+		float damagePerLevel;	
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats | Base")
+		float baseHealth;	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats | Base")
+		float baseDamage;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
